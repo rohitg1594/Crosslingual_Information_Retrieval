Fixed normalization error

utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.preprocessing import normalize
 
 
-def read_emb(path, max_vocab):
+def read_emb(path, max_vocab, norm=1):
     '''Read word embeddings from file at path.'''
     assert os.path.isfile(path)
     word2vec = {}
@@ -31,6 +31,9 @@ def read_emb(path, max_vocab):
     id2word = {v:k for k,v in word2id.items()}
     embs = np.concatenate(vectors, 0)
 
+    if norm:
+        embs = normalize(embs, axis=1, norm='l2')
+
 
     return embs, word2vec, word2id, id2word
 
@@ -43,25 +46,23 @@ def load_dictionary(path, max_vocab, word2id1, word2id2):
             if i == max_vocab:
                 break
             split = line.split()
-            id1 = int(word2id1[split[0]])
-            id2 = int(word2id2[split[1]])
-            dico_list.append([id1, id2])
+            if split[0] in word2id1 and split[1] in word2id2:
+                id1 = int(word2id1[split[0]])
+                id2 = int(word2id2[split[1]])
+                dico_list.append([id1, id2])
 
     dico = np.array(dico_list)
+
 
     return dico
 
 
-def get_parallel_data(src_embs, tgt_embs, dico, norm=True):
+def get_parallel_data(src_embs, tgt_embs, dico):
     '''Return parallel X and Y matrices corresponding to src
        and tgt embs respectively.'''
 
     X = src_embs[dico[:, 0].astype(np.int16)]
     Y = tgt_embs[dico[:, 1].astype(np.int16)]
-
-    if norm:
-        X = normalize(X, axis=1, norm='l2')
-        Y = normalize(Y, axis=1, norm='l2')
 
     return X, Y
 