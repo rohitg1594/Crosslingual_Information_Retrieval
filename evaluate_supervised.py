fix call sign configurable option in experiment files

evaluate_supervised.py
@@ -38,7 +38,7 @@ parser.add_argument("--filtersize", default=500, type=int,  help="Indicates size
 
 args = parser.parse_args()
 
-assert(args.test_sent < args.threshold)
+
 
 ###### INITIATE EVALUATION ######
 print()
@@ -226,8 +226,6 @@ def supervised_scores(model, src_encoded_sents_unsup, tgt_encoded_sents_unsup, s
     print("Starting Predictions...")
     total_scores = []
     for i in range(threshold):
-        if (i % 50) == 0: print(i,"==>", sep = " ")
-        if (i % 1000) == 0: print()
         indices = I[i]
         tgt_test = np.array(test_df['tgt'].tolist())[indices]
         src_test = np.tile(np.array(test_df['src'])[i], (filtersize, 1))

evaluate_unsupervised.py
@@ -104,7 +104,7 @@ def eval_main(W, test_dict, src_word2id, tgt_word2id, src_embs, tgt_embs, src_id
 
 
 def eval_sents(I, ks):
-    """Evaluate the sentences retrieval for top ks precision."""
+    """Evaluate the sentences retrieval for top ks precision and Mean Reciprocal Rank(MRR)."""
     topks = np.zeros(len(ks))
 
     for j, k in enumerate(ks):
@@ -120,5 +120,6 @@ def eval_sents(I, ks):
     # Mean Reciprocal Rank
     ranks = []
     for i in range(I.shape[0]):
-        ranks.append(1 / (np.where(i == I[i])[0] + 1))
+        index = np.where(i == I[i])[0] + 1
+        ranks.append(1 / index)
     print('Mean Reciprocal Rank : {}'.format(np.mean(np.array(ranks))))

unsupervised_sentences.py
@@ -121,6 +121,20 @@ else:
 # Evaluation
 index = faiss.IndexFlatIP(args.emb_dim)
 index.add(tgt_vec.astype(np.float32))
-D, I = index.search(src_vec.astype(np.float32), 100)
+D, I = index.search(src_vec.astype(np.float32), 10000)
+
+for i in range(20):
+    true = src_corpus[i]
+    true_str = ' '.join([src_id2word[id] for id in true])
+    print(true_str)
+    print('---------------------')
+    pred_str = ''
+    predictions = I[i][:10]
+    for prediction in predictions:
+        tgt_sent = tgt_corpus[prediction]
+        pred_str = ' '.join([tgt_id2word[id] for id in tgt_sent])
+        print(pred_str)
+    print('\n\n\n')
+
 
 eval_sents(I, [1, 5, 10])